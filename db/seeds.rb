1918144b7b0a016126b236b46aa7cf5d9b3af2a1
JP Lynch <JPLynch35@gmail.com>
2018-09-10 22:04:43 -0600

    add additional bars to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,3 +31,29 @@ Bar.create(
   deals: 'All 40s $5\nFireball shots $4',
   id_key: SecureRandom.hex(16)
 )
+Bar.create(
+  name: 'Oscar Blues',
+  address: '1624 Market Street, Denver, CO',
+  longitude: '-104.997508',
+  latitude: '39.750111',
+  open: { Sunday: '8am', Monday: '10am', Tuesday: '10am', Wednesday: '10am', Thursday: '10am', Friday: '10am', Saturday: '8am' },
+  close: { Sunday: '10pm', Monday: '12am', Tuesday: '12am', Wednesday: '12am', Thursday: '12am', Friday: '2am', Saturday: '2am' },
+  hh_start: { Sunday: '8am', Monday: '4pm', Tuesday: '4pm', Wednesday: '4pm', Thursday: '4pm', Friday: '4pm', Saturday: '4pm' },
+  hh_end: { Sunday: '10pm', Monday: '6pm', Tuesday: '6pm', Wednesday: '6pm', Thursday: '6pm', Friday: '6pm', Saturday: '6pm' },
+  message: 'Live music on September 19th!',
+  deals: 'All beers half off!',
+  id_key: SecureRandom.hex(16)
+)
+Bar.create(
+  name: 'Fiction Brewery',
+  address: '7101 East Colfax Avenue, Denver, CO',
+  longitude: '-104.905731',
+  latitude: '39.740381',
+  open: { Sunday: '8am', Monday: '10am', Tuesday: '10am', Wednesday: '10am', Thursday: '10am', Friday: '10am', Saturday: '8am' },
+  close: { Sunday: '10pm', Monday: '12am', Tuesday: '12am', Wednesday: '12am', Thursday: '12am', Friday: '2am', Saturday: '2am' },
+  hh_start: { Sunday: '8am', Monday: '4pm', Tuesday: '4pm', Wednesday: '4pm', Thursday: '4pm', Friday: '4pm', Saturday: '4pm' },
+  hh_end: { Sunday: '10pm', Monday: '6pm', Tuesday: '6pm', Wednesday: '6pm', Thursday: '6pm', Friday: '6pm', Saturday: '6pm' },
+  message: 'Beer slushies!',
+  deals: '$1 off all drafts',
+  id_key: SecureRandom.hex(16)
+)
