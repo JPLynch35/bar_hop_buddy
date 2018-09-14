f9af5267318748885d5b5e0ae3723db78a4e5d09
JP Lynch <JPLynch35@gmail.com>
2018-09-14 10:41:39 -0600

    refactor deals JS

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -2,7 +2,7 @@ class DealsController < ApplicationController
   def index
     respond_to do |format|
       format.html
-      format.json { render json: DealsPresenter.new(current_user.last_long, current_user.last_lat).nearby_bars }
+      format.json { render json: [current_user.last_location, DealsPresenter.new(current_user.last_long, current_user.last_lat).nearby_bars] }
     end
   end
 end
