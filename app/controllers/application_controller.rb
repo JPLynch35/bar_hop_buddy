b849fd1f585941e6fc0210078ea3a190bfd81e56
JP Lynch <JPLynch35@gmail.com>
2018-09-16 10:57:28 -0600

    add bar owner helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user, :map_display
+  helper_method :current_user, :bar_owner?
 
   def current_user
     current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def bar_owner?
+    Bar.find_by_owner_email(current_user.email) if (current_user && Bar.find_by_owner_email(current_user.email))
   end
 end
