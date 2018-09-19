265d367b292749dc6fd05c5987f6da77f2a3cf4f
JP Lynch <JPLynch35@gmail.com>
2018-09-19 08:52:08 -0600

    indexed uid column for Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180915140730) do
+ActiveRecord::Schema.define(version: 20180919144828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20180915140730) do
     t.string "last_lat"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["uid"], name: "index_users_on_uid"
   end
 
   add_foreign_key "choices", "bars"
